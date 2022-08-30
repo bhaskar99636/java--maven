3174e5d905a829378240d01a66ba1f5cebf13afd
bhaskar99636 <90154705+bhaskar99636@users.noreply.github.com>
2022-08-30 11:11:08 +0530

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -1,62 +1,19 @@
-version: 1
+def buildJar() {
+    echo "building the application..."
+    sh 'mvn package'
+} 
 
-//name: Container Build
-description: "This will create a multibranch pipeline job for container builds"
+def buildImage() {
+    echo "building the docker image..."
+    withCredentials([usernamePassword(credentialsId: 'ACR', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+        sh 'docker build -t maven--java/demo-app:jma-2.0 .'
+        sh "echo $PASS | docker login -u $USER --password-stdin"
+        sh 'docker push maven--java/demo-app:jma-2.0'
+    }
+} 
 
-type: pipeline-template
-templateType: MULTIBRANCH
+def deployApp() {
+    echo 'deploying the application...'
+} 
 
-parameters:
-  - name: github_organization
-    type: string
-    displayName: Specify the GitHub Organisation e.g. lot2learn (mandatory)
-
-  - name: github_repo
-    type: string
-    displayName: Specify the GitHub Repository e.g. nodejs-project (mandatory)
-
-  - name: headWildcardFilterIncludes
-    type: string
-    displayName: "Space-separated list of name patterns to consider. You may use * as a wildcard; for example: master release*"
-
-  - name: headWildcardFilterExcludes
-    type: string
-    displayName: "Space-separated list of name patterns to ignore even if matched by the includes list. For example: release alpha-* beta-*"
-
-  - name: github_creds
-    type: credentials
-    displayName: Specify the GitHub Credentials ID name for the source code repository (mandatory)
-
-  - name: dockerfiles
-    type: string
-    displayName: Specify the path to the Dockerfile(s) as semicolon delimited string e.g. app01/Dockerfile (optional)
-    
-  - name: build_arguments
-    type: string
-    displayName: Specify the container build arguments as semicolon delimited string e.g. VAR1=FOO;VAR2=BAR (optional)
-
-  - name: docker_tag
-    type: string
-    displayName: Specify the container tagname e.g. arnabdnany1706/smartdb:latest (mandatory)
-
-multibranch:
-  branchSource:
-    github:
-      repoOwner: ${github_organization}
-      repository: ${github_repo}
-      credentialsId: ${github_creds}
-      traits:
-        - gitHubBranchDiscovery:
-            strategyId: 3
-        - headWildcardFilter:
-            includes: ${headWildcardFilterIncludes}
-            excludes: ${headWildcardFilterExcludes}
-
-    strategy:
-      $class: DefaultBranchPropertyStrategy # All branches get the same properties
-      props:
-        - $class: NoTriggerBranchProperty # Suppress automatic SCM triggering
-
-  orphanedItemStrategy:
-    daysToKeep: 60
-  scanRepositoryInterval: 15 minutes
+return this
