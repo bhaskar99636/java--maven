7d27390368046b0bd6cec25eb907573483570db4
venkatd901 <90154705+venkatd901@users.noreply.github.com>
2022-09-18 14:25:48 +0530

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -50,7 +50,7 @@ def uploadArtifactToNexus() {
 def buildImage() {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'ACR', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t demo_java/demo-2.0-SNAPSHOT.jar .'
+        sh 'docker build -t demojava/demo-2.0-SNAPSHOT.jar .'
         sh "echo $PASS | docker login -u $USER --password-stdin"
         sh 'docker push demo_java/demo:latest'
     }
