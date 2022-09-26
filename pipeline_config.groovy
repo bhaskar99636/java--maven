5bb7540dc2617072a95667f56c97698215577f11
venkatd901 <90154705+venkatd901@users.noreply.github.com>
2022-09-26 23:51:41 +0530

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -5,7 +5,7 @@ def buildJar() {
 
 def qualityanalysis() {
     echo "sonarQube code quality check"
-    withSonarQubeEnv(credentialsId: 'mysorarqube', installationName: 'sample_java') {
+    withSonarQubeEnv(credentialsId: 'maven_java'){
     sh 'mvn sonar:sonar' 
     sh 'mvn verify sonar:sonar'
                   }
