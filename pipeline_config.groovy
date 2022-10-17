ccc126ce02efdc2f76c5d9321975d2db436a9d87
venkatd901 <90154705+venkatd901@users.noreply.github.com>
2022-10-17 15:03:00 +0530

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -22,7 +22,6 @@ def testReport(){
 
 
 def rollback() {
-    def something()
        if (something) {
            previousBuild.result = "FAILURE"
            sh "scp -o StrictHostKeyChecking=no target/demo-2.0-SNAPSHOT.jar_backup azureuser@20.219.92.67:/opt/tomcat/apache-tomcat-10.0.26/webapps"
