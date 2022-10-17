c377133843e3a3c72851dd27d7de61bf664d9d77
venkatd901 <90154705+venkatd901@users.noreply.github.com>
2022-10-17 12:57:35 +0530

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -22,18 +22,13 @@ def testReport(){
 
 
 def rollback() {
-    echo "roll back to previous version"
-    when {
-        expression { 
-            (currentBuild?.getPreviousBuild()?.result == 'FAILURE') {
-             sh "scp -o StrictHostKeyChecking=no target/demo-2.0-SNAPSHOT.jar_backup azureuser@20.219.92.67:/opt/tomcat/apache-tomcat-10.0.26/webapps"
-    if (currentBuild.resultIsBetterOrEqualTo(
-    currentBuild.getPreviousBuild().result)) {
-    echo 'build has been fixed'
-   }
-  }
-}
-    }
+       if (something) {
+           currentBuild.result = "FAILURE"
+           sh "scp -o StrictHostKeyChecking=no target/demo-2.0-SNAPSHOT.jar_backup azureuser@20.219.92.67:/opt/tomcat/apache-tomcat-10.0.26/webapps"
+       echo "roll back to previous version"
+     } else {
+           echo 'I execute next stage' 
+     }
 }
 
 def uploadArtifactToNexus() {
