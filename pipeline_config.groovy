835e24f952fd19e37f1dbf9896507141e240bcc2
venkatd901 <90154705+venkatd901@users.noreply.github.com>
2022-10-17 15:23:25 +0530

    Update pipeline_config.groovy

--- pipeline_config.groovy
+++ pipeline_config.groovy
@@ -22,10 +22,9 @@ def testReport(){
 
 
 def rollback() {
-       if {
-           (currentBuild.result = "FAILURE")
+       if (currentBuild.result = "FAILURE") {
            sh "scp -o StrictHostKeyChecking=no target/demo-2.0-SNAPSHOT.jar_backup azureuser@20.219.92.67:/opt/tomcat/apache-tomcat-10.0.26/webapps"
-       echo "roll back to previous version"
+           echo "roll back to previous version"
      } else {
            echo 'I execute next stage' 
      }
